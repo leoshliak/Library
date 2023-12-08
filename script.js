9bca631437205ca389ec8c5e394ac08ab94af16e
leoshliak <levshlyak@gmail.com>
2023-12-08 14:02:26 +0200

    design is finished

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
  
- const btn = document.querySelector('.AddBtn');
+ const SubmitBtn = document.querySelector('.AddBtn');
   const Dialog = document.querySelector('.dialog')
-
-  btn.addEventListener("click", () => {
+  const closeBtn = document.querySelector('.close');
+  const readBtn = document.querySelector('.read-green');
+  const notReadBtn = document.querySelector('notRead-red');
+  
+  SubmitBtn.addEventListener("click", () => {
      Dialog.showModal();
   });
 
-  window.addEventListener('dblclick', () => {
+  closeBtn.addEventListener('click', () => {
     Dialog.close()
-   });
+   }); 
+
+   window.addEventListener('click', (e)=> {
+    if (e.target.classList.contains("read-green")) {
+      unRead(e.target);
+    }
+  });
+   
+  function unRead(btn) {
+    if (btn.classList.contains("notRead-red")) {
+      btn.classList.remove("notRead-red");
+      btn.textContent = 'Read';
+    } else {
+      btn.classList.add("notRead-red");
+      btn.textContent = 'Not read';
+    };
+  };
 
 const myLibrary = [];
 
