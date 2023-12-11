47a0c087a9e0a8f72ffefbd0cb8103600d6fa43c
leoshliak <levshlyak@gmail.com>
2023-12-11 14:32:31 +0200

    almost done

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
  
- const SubmitBtn = document.querySelector('.AddBtn');
+  const AddtBtn = document.querySelector('.AddBtn');
   const Dialog = document.querySelector('.dialog')
   const closeBtn = document.querySelector('.close');
   const readBtn = document.querySelector('.read-green');
   const notReadBtn = document.querySelector('notRead-red');
-  
-  SubmitBtn.addEventListener("click", () => {
+  const submitBtn = document.querySelector('.form-btn');
+  const BookTitle = document.querySelector('#title');
+  const BookAuthor = document.querySelector('#author');
+  const BookPub = document.querySelector('#published');
+  const BookPages = document.querySelector('#pages');
+  const BookRead = document.querySelector('#read');
+  const form = document.querySelector('#form');
+  const cardGrid = document.querySelector('.book-grid');
+  AddtBtn.addEventListener("click", () => {
      Dialog.showModal();
   });
 
@@ -17,8 +24,9 @@
     if (e.target.classList.contains("read-green")) {
       unRead(e.target);
     }
+    
   });
-   
+
   function unRead(btn) {
     if (btn.classList.contains("notRead-red")) {
       btn.classList.remove("notRead-red");
@@ -29,12 +37,69 @@
     };
   };
 
+  function DeleteCard(element){
+    const divRemove = document.getElementById(element.parentNode.id);
+    cardGrid.removeChild(divRemove);
+    myLibrary.splice(divRemove.id, 1);
+    updateId();
+  }
+
+
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, published ,pages, read){
+   this.title = title;
+   this.author = author;
+   this.published = published;
+   this.pages = pages;
+   this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    const title = BookTitle.value;
+    const author = BookAuthor.value;
+    const published = BookPub.value;
+    const pages = BookPages.value;
+    const read = BookRead.checked;
+     myLibrary.push(new Book(title, author, published ,pages, read));
 }
+
+ function CreateCard(){
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('book-card');
+  const titleH3 = document.createElement('h3');
+  titleH3.textContent = myLibrary[myLibrary.length - 1].title;
+  const authorDate = document.createElement('p');
+  authorDate.textContent = `${myLibrary[myLibrary.length - 1].author} | ${myLibrary[myLibrary.length - 1].published}`;
+  const pagesP = document.createElement('p');
+  pagesP.textContent = `${myLibrary[myLibrary.length - 1].pages} pages`;
+  const buttonDiv = document.createElement('div');
+  buttonDiv.classList.add('card-buttons');
+  const ReadCheck = document.createElement('button');
+   if(myLibrary[myLibrary.length - 1].read == true){
+      ReadCheck.classList.add('read-green');
+      ReadCheck.textContent = 'Read'
+   } else{
+    ReadCheck.classList.add('notRead-red');
+    ReadCheck.textContent = 'Not read';
+   }
+   const deleteBtn = document.createElement('button');
+   deleteBtn.classList.add('delete');
+   deleteBtn.textContent = 'Delete';
+   cardGrid.appendChild(cardDiv);
+   cardDiv.appendChild(titleH3);
+   cardDiv.appendChild(authorDate);
+   cardDiv.appendChild(pagesP);
+   cardDiv.appendChild(buttonDiv);
+   buttonDiv.appendChild(ReadCheck);
+   buttonDiv.appendChild(deleteBtn);
+ }
+  
+
+ form.addEventListener('submit', () =>{
+  event.preventDefault();
+  addBookToLibrary();
+  CreateCard();
+  form.reset();
+ });
+ 
